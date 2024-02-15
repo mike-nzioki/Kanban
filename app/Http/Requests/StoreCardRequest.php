866573a30e77c1c1ee8dba7b51cfb90a416a342c
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 12:41:00 +0300

    implemented storeCard request

--- app/Http/Requests/StoreCardRequest.php
+++ app/Http/Requests/StoreCardRequest.php
@@ -11,7 +11,7 @@ class StoreCardRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,14 @@ class StoreCardRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'content' => ['required', 'min:10'],
         ];
+    }
+
+    public function prepareForValidation()
+    {
+        return $this->merge([
+            'column_id' => $this->column?->id,
+        ]);
     }
 }
