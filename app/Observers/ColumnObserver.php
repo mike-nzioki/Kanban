412c7b88970124d7b712463d78ca711b90109a4c
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 12:53:49 +0300

    implemented observers

---
+++ app/Observers/ColumnObserver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Column;
+
+class ColumnObserver
+{
+    public function deleting (Column $column): void
+    {
+        $column->cards()->delete();
+    }
+}
