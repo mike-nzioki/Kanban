cc3440b339723db97ef5a76258d5a92be4b0df9f
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 13:08:07 +0300

    implemented ColumnCardDestroyController

--- app/Http/Controllers/ColumnCardDestroyController.php
+++ app/Http/Controllers/ColumnCardDestroyController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
+use App\Models\Column;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ColumnCardDestroyController extends Controller
 {
-    //
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Column $column, Card $card): RedirectResponse
+    {
+        $card->delete();
+        return back();
+    }
 }
