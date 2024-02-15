214b6b5c9ae8d359b5af1a1fc614dfc6bad4bd1c
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 12:50:39 +0300

    implemeted listener

---
+++ app/Listeners/QueryExecutedListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class QueryExecutedListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        Log::channel('sql_query')->info(
+            'SQL Query',
+            [
+                $event->sql,
+                $event->bindings,
+                $event->time,
+            ]
+        );
+    }
+}
