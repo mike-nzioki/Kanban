96bcfc8630c07079341429f543f357ad03be4143
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 13:13:13 +0300

    implemented routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\BoardColumnCreateController;
 use App\Http\Controllers\BoardController;
+use App\Http\Controllers\CardsReorderUpdateController;
+use App\Http\Controllers\ColumnCardCreateController;
+use App\Http\Controllers\ColumnCardDestroyController;
+use App\Http\Controllers\ColumnCardUpdateController;
+use App\Http\Controllers\ColumnDestroyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -26,16 +32,36 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/boards/{board?}', [BoardController::class, 'show'])->name('boards');
+
+    Route::post('/boards/{board}/columns', BoardColumnCreateController::class)
+        ->name('boards.columns.store');
+
+    Route::delete('/columns/{column}', ColumnDestroyController::class)
+        ->name('columns.destroy');
+
+    Route::post('/columns/{column}/cards', ColumnCardCreateController::class)
+        ->name('columns.cards.store');
+
+    Route::put('/columns/{column}/cards/{card}', ColumnCardUpdateController::class)
+        ->scopeBindings()->name('columns.cards.update');
+
+    Route::delete('/columns/{column}/cards/{card}', ColumnCardDestroyController::class)
+        ->scopeBindings()->name('columns.cards.destroy');
+
+    Route::put('/cards/reorder', CardsReorderUpdateController::class)
+        ->name('cards.reorder');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/boards/{board}', [BoardController::class, 'show'])->name('boards');
 
 require __DIR__.'/auth.php';
