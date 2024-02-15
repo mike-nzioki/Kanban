36d494e44ce7893d351843a4f102d78955efaeb4
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 13:05:54 +0300

    implemented CardsReorderUpdateController

--- app/Http/Controllers/CardsReorderUpdateController.php
+++ app/Http/Controllers/CardsReorderUpdateController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CardsReorderUpdateRequest;
+use App\Models\Card;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CardsReorderUpdateController extends Controller
 {
-    //
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(CardsReorderUpdateRequest $request): RedirectResponse
+    {
+        $data = collect($request->columns)
+            ->recursive() // make all nested arrays a collection
+            ->map(function ($column) {
+                return $column['cards']->map(function ($card) use ($column) {
+                    return ['id' => $card['id'], 'position' => $card['position'], 'column_id' => $column['id']];
+                });
+            })
+            ->flatten(1)
+            ->toArray();
+
+        // Batch
+        Card::query()->upsert($data, ['id'], ['position', 'column_id']);
+
+        return back();
+    }
 }
