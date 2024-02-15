9d5dbcf424d9e5e01f9fcc1a475313d4e1855299
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 12:36:31 +0300

    implemented card reorder update request

--- app/Http/Requests/CardsReorderUpdateRequest.php
+++ app/Http/Requests/CardsReorderUpdateRequest.php
@@ -11,7 +11,7 @@ class CardsReorderUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class CardsReorderUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'columns' => ['nullable', 'array'],
+            'columns.*.id' => ['integer', 'required', 'exists:\App\Models\Column,id'],
+            'columns.*.cards' => ['nullable', 'array'],
+            'columns.*.cards.*.id' => ['nullable', 'integer', 'required_with:columns.*.cards', 'exists:\App\Models\Card,id'],
+            'columns.*.cards.*.position' => ['nullable', 'required_with:columns.*.cards', 'numeric'],
         ];
     }
 }
