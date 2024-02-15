7c7ed67278113028ff8866ee4e5977e805097d4b
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 13:09:54 +0300

    implemented ColumnDestroyController

--- app/Http/Controllers/ColumnDestroyController.php
+++ app/Http/Controllers/ColumnDestroyController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Column;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ColumnDestroyController extends Controller
 {
-    //
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Column $column): RedirectResponse
+    {
+        $column->delete();
+        return back();
+    }
 }
