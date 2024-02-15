ba68093462b7e85e3ac5a99b874220fd74c2a4ba
mike-nzioki <mikenzioki65@gmail.com>
2024-02-15 11:28:44 +0300

    implemented board contorller & route

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password')
+            ]
+        ];
+        foreach ($users as $user) {
+            DB::table('users')->insert($user);
+        }
+    }
+}
